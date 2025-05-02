977a10cb6c6ca1db62c3f6c59e00e1fd7c8c4c36
Mr-Ndi <onlythenotes@gmail.com>
2025-05-02 06:16:05 +0200

    Over view of vendor routes

--- Vendors/routes/vendorRoutes.go
+++ Vendors/routes/vendorRoutes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	controller "MITI_ART/Vendors/controller"
+	middlewares "MITI_ART/middleware"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -16,5 +17,18 @@ func VendorsRoutes(router *gin.Engine, db *gorm.DB) {
 		vendor.POST("/upload", func(c *gin.Context) {
 			controller.UploadHandle(c, db)
 		})
+		auth := vendor.Group("", middlewares.AuthMiddleware())
+		{
+			//Router for getting the product that belongs to the vendor
+			auth.GET("/my-products", func(c *gin.Context) {})
+			//Router for retriving a single product
+			auth.POST("/my-product/:id", func(c *gin.Context) {})
+			//Router for retriving all orders recomended from his product
+			auth.GET("/required-product", func(c *gin.Context) {})
+			//Router for editing the product description
+			auth.POST("/edit-product/:id", func(c *gin.Context) {})
+			//Router for deleting the posted product
+			auth.POST("/remove-product/:id", func(c *gin.Context) {})
+		}
 	}
 }
